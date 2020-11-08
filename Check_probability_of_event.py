65da30ade671d5ff11cca255c12b1eb03779910e
WAlexandreW <35145996+WAlexandreW@users.noreply.github.com>
2020-11-08 22:33:00 +0100

    Create Check_probability_of_event.py

---
+++ Check_probability_of_event.py
@@ -0,0 +1,17 @@
+n_sample = 1000
+
+#consider it as given
+mean_sampling_distribution = np.mean(df["CreditScore"])
+
+#consider it as given
+std_sampling_distribution = np.std(df["CreditScore"])/np.sqrt(n_sample)
+
+#any value related to the problem
+x_target_hypothesis = 651
+z_score =  (x_target_hypothesis-mean_sampling_distribution)/std_sampling_distribution
+
+#probability to be on the left/right side of the curve
+p_inferior_x_crit = stats.norm.cdf(z_score)
+p_superior_x_crit = 1- p_inferior_x_crit
+print(p_inferior_x_crit)
+print(p_superior_x_crit)
